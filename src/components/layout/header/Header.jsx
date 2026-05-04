3501d0e4c789f479a171bb834e4a95a70ebc9c8e
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-05-04 17:29:31 +0400

    change className

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,12 +1,14 @@
-import { FiArrowLeft } from 'react-icons/fi'
 import { useAuth } from '../../../hooks/useAuth'
-import styles from './Header.module.scss'
 import Hamburger from '../hamburger/Hamburger'
+import styles from './Header.module.scss'
+
+import { FiArrowLeft } from 'react-icons/fi'
+
 const Header = ({ backLink }) => {
 	const { isAuth } = useAuth()
 	return (
 		<>
-			<header className={styles.Header}>
+			<header className={styles.header}>
 				<button onClick={() => {}}>
 					<FiArrowLeft></FiArrowLeft>
 				</button>

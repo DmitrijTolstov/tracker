e6ec36b4a2ced95a61b49786018f610da6902ac6
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-05-05 08:36:06 +0400

    adding conditions for displaying unregistered users

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,18 +1,35 @@
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../hooks/useAuth'
 import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
 
 import { FiArrowLeft } from 'react-icons/fi'
+import { IoMdArrowBack } from 'react-icons/io'
+import { TfiUser } from 'react-icons/tfi'
 
-const Header = ({ backLink }) => {
+const Header = ({ backLink = '/' }) => {
 	const { isAuth } = useAuth()
+	const location = useLocation()
+	const navigate = useNavigate()
+
 	return (
 		<>
 			<header className={styles.header}>
-				<button onClick={() => {}}>
-					<FiArrowLeft></FiArrowLeft>
-				</button>
-				{/* User profile */}
+				{location.pathname !== '/' ? (
+					<button
+						onClick={() => {
+							navigate(backLink)
+						}}
+					>
+						<FiArrowLeft fill='#fff' size={27}></FiArrowLeft>
+					</button>
+				) : (
+					<>
+						<button onClick={() => navigate('/profile')}>
+							<TfiUser fill='#fff' size={27}></TfiUser>
+						</button>
+					</>
+				)}
 				<Hamburger></Hamburger>
 			</header>
 		</>

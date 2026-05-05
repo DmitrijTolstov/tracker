7ca1773ea459b3d51f26054882e9ab0b42287aed
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-05-05 08:35:03 +0400

    added style and crate homa page

--- src/components/layout/screens/home/Home.jsx
+++ src/components/layout/screens/home/Home.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-// import '../../../../App.css'
+import { use, useState } from 'react'
 import Layout from '../../Layout'
+import { useNavigate } from 'react-router-dom'
+import Button from '../../../UI/button/Button'
+import styles from './Home.module.scss'
+import { useAuth } from '../../../../hooks/useAuth'
 
 function Home() {
+	const naviagate = useNavigate()
+	const { isAuth } = useAuth()
+
 	return (
 		<>
-			<Layout>
-				<div></div>
-				<h1>Vite + React</h1>
+			<Layout bgImage={'./public/workout-images-banner.jpg'}>
+				<Button
+					clickHandler={() => naviagate(isAuth ? '/new-workout' : '/auth')}
+				>
+					{isAuth ? 'New' : 'Sign in'}
+				</Button>
+				<h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
 			</Layout>
 		</>
 	)

07cd12eeb977668c97f1c5a672505faa73fdbd1d
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-06-01 21:29:49 +0400

    added loader

---
+++ src/components/UI/loader/Loader.jsx
@@ -0,0 +1,44 @@
+const Loader = () => {
+	return (
+		<svg width='60' height='20' viewBox='0 0 120 30'>
+			<circle cx='30' cy='15' r='10' fill='#B53471'>
+				<animate
+					attributeName='cy'
+					from='15'
+					to='15'
+					dur='0.6s'
+					begin='0s'
+					repeatCount='indefinite'
+					values='15;5;15'
+					keyTimes='0;0.5;1'
+				></animate>
+			</circle>
+			<circle cx='60' cy='15' r='10' fill='#B53471'>
+				<animate
+					attributeName='cy'
+					from='15'
+					to='15'
+					dur='0.6s'
+					begin='0.2s'
+					repeatCount='indefinite'
+					values='15;5;15'
+					keyTimes='0;0.5;1'
+				></animate>
+			</circle>
+			<circle cx='90' cy='15' r='10' fill='#B53471'>
+				<animate
+					attributeName='cy'
+					from='15'
+					to='15'
+					dur='0.6s'
+					begin='0.4s'
+					repeatCount='indefinite'
+					values='15;5;15'
+					keyTimes='0;0.5;1'
+				></animate>
+			</circle>
+		</svg>
+	)
+}
+
+export default Loader

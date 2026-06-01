f7ddaa254f735cbf97863e201873d943433ba96c
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-06-01 21:30:31 +0400

    change import and names

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/styles/index.scss'
 import Router from './routes/Router'
 createRoot(document.getElementById('root')).render(
 	<StrictMode>

f1a3e3ca6330c325bcc77e034083f3e850ce58f4
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-06-05 07:45:43 +0400

    installing express-async-handler and wrapping the function with it

--- server/app/auth/auth.controller.js
+++ server/app/auth/auth.controller.js
@@ -1,7 +1,7 @@
 import { prisma } from '../prisma.js'
+import asyncHandler from 'express-async-handler'
 
-export const authUser = async (req, res) => {
-	const user = await prisma.user.findMany()
-
+export const authUser = asyncHandler(async (req, res) => {
+	const user = await prisma.user.findMany({})
 	res.json(user)
-}
+})

c5219e3faf7ee4d8c941dcc93d9a2a7918a09e8a
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-07-01 16:38:26 +0400

    adding a router for registration

--- server/app/auth/auth.routes.js
+++ server/app/auth/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { authUser } from './auth.controller.js'
+import { authUser, registerUser } from './auth.controller.js'
 
 const autRrouter = express.Router()
 
 autRrouter.route('/login').post(authUser)
+autRrouter.route('/register').post(registerUser)
 
 export default autRrouter

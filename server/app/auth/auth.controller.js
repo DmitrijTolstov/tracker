cb68fd4e10cb52b63c7599957a8700f9271f2fb4
Tolstov Dmitrii <tolstov-99@internet.ru>
2026-07-01 16:44:25 +0400

    creating and searching for customers by email and password

--- server/app/auth/auth.controller.js
+++ server/app/auth/auth.controller.js
@@ -1,7 +1,41 @@
+import { faker } from '@faker-js/faker'
 import { prisma } from '../prisma.js'
 import asyncHandler from 'express-async-handler'
+import { hash } from 'argon2'
+import { generateToken } from './generate-token.js'
+import { SelectUSerFileds } from '../utils/user.utils.js'
 
 export const authUser = asyncHandler(async (req, res) => {
 	const user = await prisma.user.findMany({})
 	res.json(user)
+})
+
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body
+
+	const isUser = await prisma.user.findUnique({
+		where: {
+			id: req.body.id,
+			email,
+			password
+		}
+	})
+
+	if (isUser) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			password: await hash(password),
+			name: faker.internet.username()
+		},
+		select: SelectUSerFileds
+	})
+
+	const token = generateToken(user.id)
+
+	res.json({ user, token })
 })
